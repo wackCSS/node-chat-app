2de16ef294baa1065164346dd28e2f3a62cb9121
Luke Edwards <wackCSS@users.noreply.github.com>
2018-05-16 18:26:33 +0100

    configure package.json

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public'); // this will remove the unnecessary ../ from the path.
 const express = require('express');
+
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
   console.log(`started on port ${port}`);
 });
-
-app.use(express.static(publicPath));
