cb87d8d3f257578b61478017eb5ba2c5d0f28054
Luke Edwards <wackCSS@users.noreply.github.com>
2018-05-21 18:38:46 +0100

    basic connect and disconnect messages

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public'); // this will remove the unnecessary ../ from the path.
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
+const publicPath = path.join(__dirname, '../public'); // this will remove the unnecessary ../ from the path.
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('connected to client');
+
+  socket.on('disconnect', (socket) => {
+    console.log('Disconnected from server');
+  });  
+});
+
+
+server.listen(port, () => {
   console.log(`started on port ${port}`);
 });
